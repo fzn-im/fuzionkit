bd40a811db2e78afb20133d7f783f782fb9859e9
pika <enzi@braindead.io>
2023-06-07 17:12:16 -0500

    chore: update shell

--- shell/shell.lit.css.js
+++ shell/shell.lit.css.js
@@ -89,7 +89,7 @@ export default css `@keyframes bars-pulse {
   top: 0;
 }
 :host(fzn-shell) > div > .app-badge > div.branding > a.up-action > .back-icon > fa-icon {
-  display: block;
+  display: flex;
   align-items: center;
   position: absolute;
   right: 5px;
